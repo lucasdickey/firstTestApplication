c2e92e748019e7a9fa70bc96522d24ee0849f086
Lucas Dickey <lucas@lucasdickey.com>
2011-10-21 20:19:47 -0700

    Test app for market publish

---
+++ src/com/snrg/test1/SnrgTest1Activity.java
@@ -0,0 +1,13 @@
+package com.snrg.test1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SnrgTest1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
